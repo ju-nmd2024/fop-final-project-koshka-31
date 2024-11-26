64031e65a418ae4c689bc5ac2a9eba5354c560c7
elinaasratyan <asel24mr@student.ju.se>
2024-11-26 15:14:56 +0100

    startScreenCat

--- game.js
+++ game.js
@@ -1,17 +1,31 @@
+var imageStartBackground;
+
 function setup() {
   createCanvas(800, 800);
+  imageStartBackground = loadImage('startScreenCat.jpg');
 }
 
 //Game start
 function gameStartScreen() {
-  background(255, 0, 255);
-  fill(0, 0, 0);
-  text("VIP KOSHKA", 400, 200);
-  text("Start", 420, 250);
+  push();
+  strokeWeight(0);
+  fill(142, 70, 61);
+  rect(530, 230, 220, 60, 30);
 
+  pop();
+
+  //text
+  push();
+  fill(255, 255, 255);
+  textSize(30);
+  text("VIP KOSHKA", 550, 270);
+  textSize(20);
+  text("Start", 625, 400);
+  pop();
 }
 
 function draw() {
+  image(imageStartBackground, 0, 0, 800, 800);
   gameStartScreen();
 
 
